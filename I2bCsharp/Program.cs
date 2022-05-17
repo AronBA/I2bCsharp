8779a94bc9357c0709633fba6fb20532c3d78c92
AronBA <72823328+AronBA@users.noreply.github.com>
2022-05-17 08:24:45 +0200

    Projektdateien hinzufügen.

---
+++ I2bCsharp/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace I2bCsharp
+{
+    class Program
+    {
+       static void Main(string[] args)
+        {
+            Console.WriteLine("Test");
+        }
+            
+    }
+}
